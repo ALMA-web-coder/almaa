b510cb815a957145ef76631a278b21dde79a0153
guineveretim <168146144+guineveretim@users.noreply.github.com>
2025-05-28 14:22:31 +0200

    Update

--- .history/core/settings_20250528142206.py
+++ .history/core/settings_20250528142206.py
@@ -19,13 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-
+"""""
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-i8(-4%4an#y%06n+-zr%e6@d8$^c6ynj=o716fsom9@qk(a$2y'
+"""""
 
-"""""
 SECRET_KEY = os.environ.get('SECRET_KEY')
-"""""
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
